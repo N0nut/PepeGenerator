363dc0a69039773d9cef1e59beebd5817061371e
N0nut <68543697+N0nut@users.noreply.github.com>
2021-01-09 22:22:20 +0100

    created tkinter canvas, imported PIL, imageio, sys, tkinter

---
+++ PepeGenerator.py
@@ -0,0 +1,13 @@
+import PIL
+import imageio
+import sys
+
+from tkinter import *
+
+
+if __name__ == "__main__":
+    root = Tk()
+    canvas = Canvas(root, width=750, height=750)
+    canvas.pack()
+    mainloop()
+    sys.exit()
